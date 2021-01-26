4cd3095b6d477478b0492d68d895e05543e7728e
Meredith Jones <mj19003@gmail.com>
2021-01-26 16:19:38 -0500

    Added server.js file with dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+//Requiring dependencies: express, mongoose, and morgan 
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan  = require('morgan')
+
+//set up the port
+const PORT = process.env.PORT || 3000;
+
