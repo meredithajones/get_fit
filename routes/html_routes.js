ce4ba0e0778776e7eea441d3cd92b9eed9e96095
Meredith Jones <mj19003@gmail.com>
2021-01-26 20:26:40 -0500

    Adding dependencies for html routes

---
+++ routes/html_routes.js
@@ -0,0 +1,9 @@
+// Set up dependencies
+const router = require("express").Router();
+//We need "path" to access the static files for the html and create the routes.
+const path = require("path");
+
+//exporting router
+module.exports = router;
+
+   
