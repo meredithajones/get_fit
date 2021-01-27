fcf3478c0cf9eda126a406358ad409d3da2e79fa
Meredith Jones <mj19003@gmail.com>
2021-01-27 15:41:56 -0500

    deploying

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,16 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbGetFit",
+{
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+}
+);
+
+
 
 let workoutSeed = [
   {
